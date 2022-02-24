436ea10e6112356e4d643b65867a0412e0b0af2a
Drappear <44987855+Drappear@users.noreply.github.com>
2022-02-24 18:19:24 +0900

    0224

---
+++ src/chap02/lecture/A02Variable.java
@@ -0,0 +1,15 @@
+package chap02.lecture;
+
+public class A02Variable {
+	public static void main(String[] args) {
+		// 변수의 사용 범위(scope) : 선언된 블럭{} 안
+		
+		boolean b = true;
+//		int a = 4;
+		if (b) {
+			int a =3;
+			System.out.println(a);
+		}
+//		System.out.println(a);
+	}
+}
---
+++ src/chap02/lecture/A07TypeChar.java
@@ -0,0 +1,16 @@
+package chap02.lecture;
+
+public class A07TypeChar {
+	public static void main(String[] args) {
+		// 문자형 : char(2)
+		char var1 = ' ';
+//		char var2 = '';  x
+		char var3 = '\\';
+		char var4 = 'a';
+		char var5 = '가';
+		char var6 = '나';
+		char var7 = '\uac00';
+		
+		System.out.println(var7);
+	}
+}
