750d13c32e58c02bc338299a31977fa390b91629
Drappear <44987855+Drappear@users.noreply.github.com>
2022-03-03 02:10:55 +0900

    Exercise

---
+++ src/chap04/book/exercise/Exercise06.java
@@ -0,0 +1,24 @@
+package chap04.book.exercise;
+
+public class Exercise06 {
+	public static void main(String[] args) {
+		String star = "*";
+		for (int i = 0; i < 5; i++) {
+			System.out.println(star);
+			star += "*";
+		}
+		
+		System.out.println("출력 (끝에 enter없이)");
+		
+		System.out.print("출력1");
+		System.out.print("출력2");
+		System.out.print("출력3");
+		System.out.println("출력4");
+		System.out.println("출력5");
+		
+		for (int i = 0; i < 5; i++) {
+			
+		}
+	}	
+	
+}
