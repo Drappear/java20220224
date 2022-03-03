6129e2d860bde1de4e0414743b52a5c57fd10276
Drappear <44987855+Drappear@users.noreply.github.com>
2022-03-03 14:34:57 +0900

    break continue

---
+++ src/chap04/lecture/breakcontinue/E01Break.java
@@ -0,0 +1,12 @@
+package chap04.lecture.breakcontinue;
+
+public class E01Break {
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			if (i == 5) {
+				break;				
+			}
+			System.out.println(i);
+		}
+	}
+}
