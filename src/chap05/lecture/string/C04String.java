f8727e3b61394bf83f77807ec69d062cf6bb568c
Drappear <44987855+Drappear@users.noreply.github.com>
2022-03-08 17:30:36 +0900

    string

---
+++ src/chap05/lecture/string/C04String.java
@@ -0,0 +1,25 @@
+package chap05.lecture.string;
+
+public class C04String {
+	public static void main(String[] args) {
+		// char + String
+		char c1 = '가';
+		String s1 = "나";
+		
+		
+		System.out.println(c1 + s1);
+		
+		String s2 = c1 + s1;
+		
+		System.out.println(s2);
+		
+		// char + char
+		char c2 = 'a';
+		char c3 = 'b';
+
+		System.out.println(c2+c3);
+		
+		int i = c2 + c3;
+		System.out.println(i);
+	}
+}
