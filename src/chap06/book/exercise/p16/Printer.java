59347b17df0879ef9ec3e5b9a72fce14a97ddd76
Drappear <44987855+Drappear@users.noreply.github.com>
2022-03-11 17:28:32 +0900

    exercise

---
+++ src/chap06/book/exercise/p16/Printer.java
@@ -0,0 +1,16 @@
+package chap06.book.exercise.p16;
+
+public class Printer {
+	public void println(int num) {
+		System.out.println(num);		
+	}
+	public void println(boolean b) {
+		System.out.println(b);
+	}
+	public void println(double db) {
+		System.out.println(db);
+	}
+	public void println(String str) {
+		System.out.println(str);
+	}
+}
---
+++ src/chap06/book/exercise/p17/Printer.java
@@ -0,0 +1,16 @@
+package chap06.book.exercise.p17;
+
+public class Printer {
+	static void println(int num) {
+		System.out.println(num);		
+	}
+	static void println(boolean b) {
+		System.out.println(b);
+	}
+	static void println(double db) {
+		System.out.println(db);
+	}
+	static void println(String str) {
+		System.out.println(str);
+	}
+}
