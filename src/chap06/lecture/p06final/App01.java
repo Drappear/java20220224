ec340d183783f3ad99c928cf35d89db213ee5adc
Drappear <44987855+Drappear@users.noreply.github.com>
2022-03-11 17:30:20 +0900

    final

---
+++ src/chap06/lecture/p06final/App01.java
@@ -0,0 +1,26 @@
+package chap06.lecture.p06final;
+
+public class App01 {
+	public static void main(String[] args) {
+		// final : 변수 선언시 작성하면 값을 한번만 할당할 수 있음
+		
+		int i = 0;
+		i = 3;
+		
+		final int j = 0;
+//		j = 1;  // x
+		
+		final int k;
+		k = 9;
+//		k = 10; // x
+	
+		method1(3);
+		method1(10);
+	}
+	
+	
+	public static void method1(final int i) {
+//		i = 30;	// x method호출 시 값이 할당되기 때문
+		
+	}
+}
