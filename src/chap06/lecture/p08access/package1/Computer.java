d6b37704139a28604cde61aa2a4891c0081eac4c
Drappear <44987855+Drappear@users.noreply.github.com>
2022-03-15 02:00:23 +0900

    0314

---
+++ src/chap06/lecture/p08access/package1/Computer.java
@@ -0,0 +1,7 @@
+package chap06.lecture.p08access.package1;
+
+public class Computer {
+	String str1;			// default(package private) 접근 제한자
+	private String str2;	// private : package내에서 접근 가능
+	public String str3;		// public : 모두 접근 가능
+}
